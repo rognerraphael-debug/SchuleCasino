ade8992fcad6f691fb884e19dea9c9cb575ab255
Rogner <rogner.raphael@gmail.com>
2026-04-29 16:39:50 +0200

    Initial sharing of project

---
+++ Sample.java
@@ -0,0 +1,34 @@
+import ea.*;
+
+public class Sample extends Game {
+    public static void main(String[] args) {
+        new Sample();
+    }
+
+    private Bild box;
+    public Sample() {
+        super(1920 , 1080,false);
+
+        box = new Bild(10,10,"tomas.png");
+
+        wurzel.add(box);
+    }
+
+    @Override
+    public void tasteReagieren(int code) {
+        switch(code) {
+            case Taste.OBEN:   box.verschieben(+0, -10); break;
+            case Taste.RECHTS: box.verschieben(+10, +0); break;
+            case Taste.UNTEN:  box.verschieben(+0, +10); break;
+            case Taste.LINKS:  box.verschieben(-10, +0); break;
+        }
+    }
+    
+    public void test() {
+        while (tasteGedrueckt(1) == false) {
+            System.out.println("W");
+        }
+    }
+    
+    
+}
